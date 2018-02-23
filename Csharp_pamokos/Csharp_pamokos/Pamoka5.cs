3e25c64de786522dd30d8067e853892ab7ab7129
Donce <d.zaleckis@gmail.com>
2018-02-23 23:17:35 +0200

    no message

--- Csharp_pamokos/Csharp_pamokos/Pamoka5.cs
+++ Csharp_pamokos/Csharp_pamokos/Pamoka5.cs
@@ -10,6 +10,89 @@ namespace Csharp_pamokos
     {
         static void Main(string[] args)
         {
+            //pratimas1();
+            //pratimas2();
+            pratimas3();
         }
+
+        static void pratimas1()
+        {
+            var number = 5;
+            Console.WriteLine("start " + number);
+            increaseNormal(number);
+            Console.WriteLine("after normal " + number);
+            increaseRef(ref number);
+            Console.WriteLine("after ref: " + number);
+            increaseOut(out number);
+            Console.WriteLine("after out: " + number);
+            Console.ReadLine();
+        }
+
+        static void increaseNormal(int number)
+        {
+            number++;
+        }
+
+        static void increaseRef(ref int number)
+        {
+            number++;
+        }
+
+        static void increaseOut(out int number)
+        {
+            number = 4;
+            number++;
+        }
+
+        static Boolean pratimas2()
+        {
+            string zodis = Console.ReadLine();
+            for (int i = 0; i < zodis.Length; i++)
+            {
+                char simbolis = zodis[i];
+                if (simbolis != '0' && simbolis != '1' && simbolis != '2' && simbolis != '3' && simbolis != '4' && simbolis != '5' && simbolis != '6' && simbolis != '7' && simbolis != '8' && simbolis != '9')
+                {
+                    if (simbolis == '-' || i != 0 )
+                    {
+                        return false;
+                    }
+                    
+                }
+
+            }
+            Console.ReadLine();
+            return true;
+
+        }
+
+        static void pratimas3()
+        {
+            /*
+            Console.WriteLine("irasyk 5 zodzius");
+            string zodis1 = Console.ReadLine();
+            string zodis2 = Console.ReadLine();
+            string zodis3 = Console.ReadLine();
+            string zodis4 = Console.ReadLine();
+            string zodis5 = Console.ReadLine();
+            string[] Masyvas1 = new string[5] { zodis1, zodis2, zodis3, zodis4, zodis5 };
+            for (int i = 0; i < Masyvas1.Length; i++)
+            {
+                Console.WriteLine(Masyvas1[i] + " ");
+                Console.ReadLine();
+            }
+            for (int i = 0; i < Masyvas1.Length; i++)
+            {
+                Console.WriteLine(zodis1 + " " + zodis2 + " " + zodis3 + " " + zodis4 + " " + zodis5);
+                Console.ReadLine();
+            }
+            */
+            // check Irina's Gist
+
+            string[] data = { "Mano", "batai", "buvo", "Mano", "batai", "buvo", "du", "buvo", "du", "." };
+            
+
+        }
+
+
     }
 }
